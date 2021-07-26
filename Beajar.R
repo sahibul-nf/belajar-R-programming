ea34cfebb42c07b8ed99085af318d0f139e8db84
sahibul-nf <sahibulnuzulfirdaus13@gmail.com>
2021-07-26 09:21:21 +0000

    Add Fungsi & Operator & List source code docs

---
+++ Beajar.R
@@ -0,0 +1,18 @@
+
+usia = 20
+
+if (usia < 20) {
+  print("remaja")
+} else {
+  print("dewasa")
+}
+
+dataAngka = c(10, 100, 50, 40)
+
+for (i in dataAngka) {
+  print(i)
+}
+
+for(i in dataAngka2) {
+  print(i)
+}
